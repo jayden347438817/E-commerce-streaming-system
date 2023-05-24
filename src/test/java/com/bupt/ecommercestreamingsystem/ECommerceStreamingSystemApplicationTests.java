74589ade6fb0ffbacb49af305bc93820f150b147
JaydenChan <347438817@qq.com>
2023-05-24 18:01:56 +0800

    创建项目
    还未初始化

---
+++ src/test/java/com/bupt/ecommercestreamingsystem/ECommerceStreamingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bupt.ecommercestreamingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceStreamingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
